5874c56e1096dc76afc8bbbe533ab299376ff8c3
Masoud Darvishian <masoud.darvishian@etermax.de>
2024-01-06 20:46:57 +0100

    Added and filled cube_points array

--- MyRenderer/Main.c
+++ MyRenderer/Main.c
@@ -5,11 +5,25 @@
 #include "display.h"
 #include "vector.h"
 
+#define N_POINTS 9 * 9 * 9 // acts like a const variable
+
+vec3_t cube_points[N_POINTS];
+
 bool is_running = false;
 
 void setup(void) {
 	color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 	color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
+
+	int point_count = 0;
+	for (float x = -1; x <= 1; x += 0.25) {
+		for (float y = -1; y <= 1; y += 0.25) {
+			for (float z = -1; z <= 1; z += 0.25) {
+				vec3_t new_point = { x, y, z };
+				cube_points[point_count++] = new_point;
+			}
+		}
+	}
 }
 
 void process_input(void) {
