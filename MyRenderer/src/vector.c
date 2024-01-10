5aa410e86d7849db51cbc4425976bcf17defd10b
Masoud Darvishian <masoud.darvishian@etermax.de>
2024-01-10 15:44:39 +0100

    Added vector multiply and divide functions

--- MyRenderer/src/vector.c
+++ MyRenderer/src/vector.c
@@ -15,6 +15,14 @@ vec2_t vec2_sub(vec2_t a, vec2_t b) {
 	vec2_t result = { .x = a.x - b.x, .y = a.y - b.y };
 	return result;
 }
+vec2_t vec2_mul(vec2_t v, float factor) {
+	vec2_t result = { .x = v.x * factor, .y = v.y * factor };
+	return result;
+}
+vec2_t vec2_div(vec2_t v, float factor) {
+	vec2_t result = { .x = v.x / factor, .y = v.y / factor };
+	return result;
+}
 
 ////////////////////////////////////////////////////////////
 // Vector 3D functions
@@ -28,6 +36,14 @@ vec3_t vec3_add(vec3_t a, vec3_t b) {
 }
 vec3_t vec3_sub(vec3_t a, vec3_t b) {
 	vec3_t result = { .x = a.x - b.x, .y = a.y - b.y, .z = a.z - b.z };
+	return result;
+}
+vec3_t vec3_mul(vec3_t v, float factor) {
+	vec3_t result = { .x = v.x * factor, .y = v.y * factor, .z = v.z * factor };
+	return result;
+}
+vec3_t vec3_div(vec3_t v, float factor) {
+	vec3_t result = { .x = v.x / factor, .y = v.y / factor, .z = v.z / factor };
 	return result;
 }
 
