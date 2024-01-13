2b52caf222dd476bc4687ff4ac464ece8ac8f6f9
Masoud Darvishian <masoud.darvishian@etermax.de>
2024-01-13 20:53:53 +0100

    Filled triangles

--- MyRenderer/src/triangle.c
+++ MyRenderer/src/triangle.c
@@ -1,7 +1,7 @@
 #include "triangle.h"
 #include "display.h"
 
-void vec2_swap(int* a, int* b) {
+void int_swap(int* a, int* b) {
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
@@ -20,7 +20,7 @@ void vec2_swap(int* a, int* b) {
 //  (x1,y1)------(x2,y2)
 //
 ///////////////////////////////////////////////////////////////////////////////
-void fill_flat_bottom_trinalge(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+void fill_flat_bottom_trialge(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
 	float inv_slope_1 = (float)(x1 - x0) / (y1 - y0);
 	float inv_slope_2 = (float)(x2 - x0) / (y2 - y0);
 
@@ -47,8 +47,18 @@ void fill_flat_bottom_trinalge(int x0, int y0, int x1, int y1, int x2, int y2, u
 //        (x2,y2)
 //
 ///////////////////////////////////////////////////////////////////////////////
-void fill_flat_top_trinalge(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+void fill_flat_top_trialge(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+	float inv_slope_1 = (float)(x2 - x0) / (y2 - y0);
+	float inv_slope_2 = (float)(x2 - x1) / (y2 - y1);
 
+	float x_start = x2;
+	float x_end = x2;
+
+	for (int y = y2; y >= y1; y--) {
+		draw_line(x_start, y, x_end, y, color);
+		x_start -= inv_slope_1;
+		x_end -= inv_slope_2;
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -77,24 +87,36 @@ void fill_flat_top_trinalge(int x0, int y0, int x1, int y1, int x2, int y2, uint
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
 	// we need to sort the vertices by y-coordinate ascending (y0 < y1 < y2)
 	if (y0 > y1) {
-		vec2_swap(&y0, &y1);
+		int_swap(&y0, &y1);
+		int_swap(&x0, &x1);
 	}
 	if (y1 > y2) {
-		vec2_swap(&y1, &y2);
+		int_swap(&y1, &y2);
+		int_swap(&x1, &x2);
 	}
-	// double check
 	if (y0 > y1) {
-		vec2_swap(&y0, &y1);
+		int_swap(&y0, &y1);
+		int_swap(&x0, &x1);
 	}
 
-	// Calculate the new vertex (Mx, My) using triangle similarity
-	int My = y1;
-	int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+	if (y1 == y2) {
+		// Draw flat-bottom triangle
+		fill_flat_bottom_trialge(x0, y0, x1, y1, x2, y2, color);
+	}
+	else if (y0 == y1) {
+		// Draw flat-top triangle
+		fill_flat_top_trialge(x0, y0, x1, y1, x2, y2, color);
+	}
+	else {
+		// Calculate the new vertex (Mx,My) using triangle similarity
+		int My = y1;
+		int Mx = (((x2 - x0) * (y1 - y0)) / (y2 - y0)) + x0;
 
-	// Draw flat-bottom triangle
-	fill_flat_bottom_trinalge(x0, y0, x1, y1, Mx, My, color);
+		// Draw flat-bottom triangle
+		fill_flat_bottom_trialge(x0, y0, x1, y1, Mx, My, color);
 
-	// Draw flat-top triangle
-	fill_flat_top_trinalge(x1, y1, Mx, My, x2, y2, color);
+		// Draw flat-top triangle
+		fill_flat_top_trialge(x1, y1, Mx, My, x2, y2, color);
+	}
 }
 
