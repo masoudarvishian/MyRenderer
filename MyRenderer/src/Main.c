cb125d9438c31651b622d4dfe86d709f0d63f35d
Masoud Darvishian <masoud.darvishian@etermax.de>
2024-01-21 09:49:29 +0100

    Added translation matrix operation

--- MyRenderer/src/Main.c
+++ MyRenderer/src/Main.c
@@ -82,10 +82,13 @@ void update(void) {
 	mesh.rotation.y += 0.01;
 	mesh.rotation.z += 0.01;
 
-	mesh.scale.x += 0.002;
-	mesh.scale.y += 0.001;
+	/*mesh.scale.x += 0.002;
+	mesh.scale.y += 0.001;*/
+	mesh.translation.x += 0.01;
+	mesh.translation.z = 5.0;
 
 	mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
+	mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
 
 	// Loop all triangle faces of our mesh
 	int num_faces = array_length(mesh.faces);
@@ -105,9 +108,8 @@ void update(void) {
 
 			// Use a matrix to scale our original vertex
 			transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
+			transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
 
-			// Translate the vertex away from the camera
-			transformed_vertex.z += 5;
 			transformed_vertices[j] = transformed_vertex;
 		}
 
