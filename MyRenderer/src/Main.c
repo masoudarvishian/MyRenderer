2f8c7220d727efd9145d2c016d73b84bd72b83a1
Masoud Darvishian <masoud.darvishian@etermax.de>
2024-01-23 09:00:36 +0100

    Added and applied rotation matrices

--- MyRenderer/src/Main.c
+++ MyRenderer/src/Main.c
@@ -82,13 +82,16 @@ void update(void) {
 	mesh.rotation.y += 0.01;
 	mesh.rotation.z += 0.01;
 
-	/*mesh.scale.x += 0.002;
-	mesh.scale.y += 0.001;*/
+	//mesh.scale.x += 0.002;
+	//mesh.scale.y += 0.001;
 	mesh.translation.x += 0.01;
 	mesh.translation.z = 5.0;
 
 	mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
 	mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
+	mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
+	mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
+	mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
 
 	// Loop all triangle faces of our mesh
 	int num_faces = array_length(mesh.faces);
@@ -106,10 +109,12 @@ void update(void) {
 		for (int j = 0; j < 3; j++) {
 			vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-			// Use a matrix to scale our original vertex
 			transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
+			transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+			transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
+			transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
 			transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
-
+			
 			transformed_vertices[j] = transformed_vertex;
 		}
 
